52be1096d511d21fa85ad69c64a132072b209d4c
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-26 14:57:53 -0400

    initial get routes for html docs

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const PORT = process.env.PORT || 3001;
 const app = express ();
 
 // get route for index.html
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 // get route for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log ('Active server on port 3001!');
