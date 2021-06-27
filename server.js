0758ed1fce9fec560f6ac3b9fb1ef2244efb6746
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-26 22:33:33 -0400

    updated routes, adding route for stored notes

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
+// modules and requirements
 const fs = require ('fs');
 const express = require ('express');
+const path = require ('path');
+const db = require ('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express ();
 
+// middleware for handling incoming notes and serving static files
+app.use (express.urlencoded({ extended: true }));
+app.use (express.static('public'));
+app.use(express.json());
+
+// get route for stored notes
+app.get('/api/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
 // get route for index.html
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 // get route for notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+// 'wildcard' routing
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log ('Active server on port 3001!');
+    console.log (`Active server on port ${PORT}!`);
 });
