194d0741e6c195c92ed686253f66a77e0d7c554d
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-27 01:12:24 -0400

    corrections to routing

--- routes/routing.js
+++ routes/routing.js
@@ -10,15 +10,15 @@ router.get ('/notes', (req, res) => {
 // post route for adding new notes
 router.post ('/notes', (req, res) => {
     // parse current notes
-    let currentNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    let currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     // declare new note
     let newNotes = req.body;
     // establish id of new note
-    newNotes.id = saved.length;
+    newNotes.id = currentNotes.length;
     // push new note to notes db file
     currentNotes.push(newNotes);
 
-    fs.writeFile('.db/db.json', JSON.stringify(currentNotes), err => {
+    fs.writeFile('./db/db.json', JSON.stringify(currentNotes), err => {
         if (err) throw err;
         console.log('Note Saved!');
         res.json(currentNotes);
@@ -28,11 +28,11 @@ router.post ('/notes', (req, res) => {
 // delete saved notes???
 router.delete ('/notes/:id', (req, res) => {
     // parse current notes
-    let currentNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    let currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     // find note by id?
 
     // rewrite remaining notes to db file
-    fs.writeFile('.db/db.json', JSON.stringify(currentNotes), err => {
+    fs.writeFile('./db/db.json', JSON.stringify(currentNotes), err => {
         if (err) throw err;
         console.log('Note Removed!');
         res.json(currentNotes);
