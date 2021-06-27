46608d8ffa349333ee416e37051833509b6485be
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-06-26 23:42:37 -0400

    adding new routes and function for adding notes

--- routes/routing.js
+++ routes/routing.js
@@ -4,16 +4,23 @@ const router = require ('express').Router();
 // === API Routing ===
 
 // get route for stored notes
-router.get('/notes', (req, res) => {
+router.get ('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
 });
 
 // post route for adding new notes
-router.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+router.post ('/notes', (req, res) => {
+    // parse current notes
+    let currentNotes = JSON.parse(fs.readFile('../db/db.json', 'utf8'));
+    // declare new note
+    let newNotes = req.body;
+    // establish id of new note
+    newNotes.id = saved.length;
+    // push new note to notes db file
+    currentNotes.push(newNotes);
 });
 
 // delete saved notes???
+router.delete ('.')
 
 module.exports = router;
